2755c1eaf124e22b87f8f74b6dff2c907f46f43b
eespinoz4 <eespinoza@netcore.cl>
2024-01-04 09:51:02 -0300

    Models y ViewModels creados

---
+++ Engine/Models/ViewModels/GameSession.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Engine.Models;
+
+namespace Engine.Models.ViewModels
+{
+    class GameSession
+    {
+        Player CurrentPlayer { get; set; }
+        public GameSession()
+        {
+            CurrentPlayer = new Player();
+            CurrentPlayer.Name = "Scott";
+            CurrentPlayer.Gold = 1000000;
+        }
+    }
+}
